a840bc682cf53e5ec2d9e456e586c3d210b2b051
kc1116 <kclaybon1@gmail.com>
2019-03-17 07:12:44 -0400

    update generated proto files

--- protos/gen_protos.sh
+++ protos/gen_protos.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+protoc -I=. templates/template.proto templates/payload.proto templates/transaction_receipts.proto --go_out=.
+protoc -I=. credentials/credential.proto  credentials/payload.proto credentials/transaction_receipts.proto --go_out=.
+
+mv ./github.com/BadgeForce/sawtooth-utils/protos/credentials_pb ./credentials_pb
+mv ./github.com/BadgeForce/sawtooth-utils/protos/templates_pb ./templates_pb
